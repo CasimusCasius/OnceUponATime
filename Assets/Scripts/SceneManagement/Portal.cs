b74fe668fb1af9805a313ed262018d86c94559c5
Grzegorz Kazimierczak <grzegorz.kazimierczak.sg@gmail.com>
2023-07-02 00:29:57 +0200

    Portal spawn points

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 namespace Game.SceneManagement
@@ -8,13 +10,48 @@ namespace Game.SceneManagement
 	public class Portal : MonoBehaviour
 	{
         [SerializeField] private int sceneToLoadIndex = -1;
+        [SerializeField] private Transform spawnPoint;
+
+        private void Awake()
+        {
+            DontDestroyOnLoad(this);
+        }
         private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.CompareTag("Player"))
             {
-                print("Portal triggered");
-                SceneManager.LoadScene(sceneToLoadIndex);
+                StartCoroutine(Transition());
             }
+        }
+
+        private IEnumerator Transition()
+        {
+
+            
+            yield return SceneManager.LoadSceneAsync(sceneToLoadIndex);
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal.spawnPoint);
+
+            Destroy(gameObject);
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach (Portal portal in GameObject.FindObjectsOfType<Portal>())
+            {
+                if (portal == this) { continue; }
+                return portal;
+            }
+            return null;
+        }
+
+        private void UpdatePlayer(Transform portalSpawnPoint)
+        {
+            var player = GameObject.FindWithTag("Player");
+
+            player.GetComponent<NavMeshAgent>().Warp(portalSpawnPoint.position);
+            player.transform.rotation = portalSpawnPoint.rotation;
         }
     }
 
