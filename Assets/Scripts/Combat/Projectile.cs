b10b903d62e93ba78ddbf3e7c286d83c05467a20
Grzegorz Kazimierczak <grzegorz.kazimierczak.sg@gmail.com>
2023-07-20 22:41:58 +0200

    Homing and notHoming projectiles

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -1,6 +1,4 @@
 using Game.Core;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Game.Combat
@@ -9,14 +7,21 @@ namespace Game.Combat
     {
         [SerializeField] private Health target = null;
         [SerializeField] private float speed = 1;
+        [SerializeField] private bool isHoming = false;
         float damage = 0;
 
+        private void Start()
+        {
+            transform.LookAt(target.transform.position);
+            Destroy(gameObject, 5f);
+        }
         // Update is called once per frame
         void Update()
         {
-            if (target == null) return;
-
-            transform.LookAt(target.transform.position);
+            if (target != null && isHoming && target.IsAlive()) return;
+            {
+                transform.LookAt(target.transform.position);
+            }
             transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
 
@@ -29,12 +34,11 @@ namespace Game.Combat
         private void OnTriggerEnter(Collider other)
         {
             var candidateTarget = other.GetComponent<Health>();
-            if (candidateTarget != null && candidateTarget == target ) 
+            if (candidateTarget != null && candidateTarget == target && candidateTarget.IsAlive())
             {
                 target.TakeDamage(damage);
-
+                Destroy(gameObject);
             }
-            Destroy(gameObject);
 
         }
     }
